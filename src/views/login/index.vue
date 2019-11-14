4da9fd6d1960e79a15c8b18bfc7351c99fb42fe1
dishuichuanshi <1253867871@qq.com>
2019-11-14 23:49:30 +0800

    登录模块-安装axios-配置全局使用

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,19 +7,19 @@
 </div>
   <!-- 卡片内容 -->
   <!--表单 -->
-  <el-form style="margin-top:10px">
+  <el-form style="margin-top:10px" :model="loginForm" :rules='loginRules'>
     <!-- 一个表单域就有一个el-form-item -->
-    <el-form-item>
+    <el-form-item prop='mobile'>
       <!-- 手机号 -->
-      <el-input></el-input>
+      <el-input v-model="loginForm.mobile"></el-input>
     </el-form-item>
     <!-- 短信验证 -->
-    <el-form-item>
-      <el-input style="width:250px"></el-input>
+    <el-form-item prop='code'>
+      <el-input style="width:250px" v-model="loginForm.code" ></el-input>
        <el-button style="float:right" plain>发送验证码</el-button>
     </el-form-item>
-      <el-form-item>
-        <el-checkbox>我已阅读并同意次条款内容</el-checkbox>
+      <el-form-item prop='code'>
+        <el-checkbox v-model="loginForm.check">我已阅读并同意次条款内容</el-checkbox>
       </el-form-item>
        <el-form-item>
           <el-button style="width:100%  " type="primary">登录</el-button>
@@ -34,6 +34,16 @@
 
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: ''
+      },
+      loginRules: {}
+    }
+  }
 
 }
 </script>
